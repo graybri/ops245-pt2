1b896f4380aef64bb3dfd5bdc90858714dbae55a
Brian Gray <graybri@gmail.com>
2023-11-24 03:25:41 -0500

    fix perms

--- setlinks.sh
+++ setlinks.sh
@@ -8,11 +8,11 @@ then
     exit 1
 fi
 
-ln -s /root/.bin/ops245-pt2/resettest.sh /usr/local/bin/resettest
-ln -s /root/.bin/ops245-pt2/starttest.sh /usr/local/bin/starttest
-ln -s /root/.bin/ops245-pt2/testsetup.sh /usr/local/bin/testsetup
-ln -s /root/.bin/ops245-pt2/neton.sh /usr/local/bin/neton
-ln -s /root/.bin/ops245-pt2/netoff.sh /usr/local/bin/netoff
-ln -s /root/.bin/ops245-pt2/setlinks.sh /usr/local/bin/setlinks
+ln -s -f /root/.bin/ops245-pt2/resettest.sh /usr/local/bin/resettest
+ln -s -f /root/.bin/ops245-pt2/starttest.sh /usr/local/bin/starttest
+ln -s -f /root/.bin/ops245-pt2/testsetup.sh /usr/local/bin/testsetup
+ln -s -f /root/.bin/ops245-pt2/neton.sh /usr/local/bin/neton
+ln -s -f /root/.bin/ops245-pt2/netoff.sh /usr/local/bin/netoff
+ln -s -f /root/.bin/ops245-pt2/setlinks.sh /usr/local/bin/setlinks
 
 
